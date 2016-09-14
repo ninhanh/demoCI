367ebe17175a2475f30b38162e01f3682ed39a58
AnhNN <anh.nguyen.ninh@ntq-solution.com.vn>
2016-09-14 14:59:03 +0700

    Create

---
+++ DemoTeamCity/DemoTeamCity/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace DemoTeamCity
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(1 == 2);
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+    }
+}
